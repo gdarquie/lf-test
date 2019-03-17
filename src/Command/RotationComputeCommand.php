2454ee38699fc7053dc6f901da6abdaeef08a903
gdarquie <gaetan.darquie@gmail.com>
2019-03-17 16:27:53 +0100

    add comment

--- src/Command/RotationComputeCommand.php
+++ src/Command/RotationComputeCommand.php
@@ -4,12 +4,18 @@ namespace App\Command;
 
 use App\Domain\ComputeRotation;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * We used here a procedure to show we can have different possibilities to integrate data, this way is very performant but it is not directly in the php application : we use plpgsql function to compute the rotation and update products data.
+ *
+ * Using plpgsql can be usefull if we would like to manage millions of data, but in this test it is not required, I change wanted to show a way for optimization if it is needed one day
+ *
+ * Class RotationComputeCommand
+ * @package App\Command
+ */
 class RotationComputeCommand extends Command
 {
     protected static $defaultName = 'app:rotation:compute';
